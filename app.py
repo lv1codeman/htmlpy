bdfedceb552e33fd5604ec7e42a335ea0ecd17f6
lv1codeman <lv1codeman@gmail.com>
2024-03-21 14:38:17 +0800

    demo ajax

--- app.py
+++ app.py
@@ -1,19 +1,40 @@
-from flask import Flask, render_template
-import render
+from flask import Flask, render_template, request, jsonify, json
 app = Flask(__name__)
 
 
-@app.route('/')
-# def index():
-#     # 在这个例子中，我们将向HTML传递一个Python列表
-#     items = ["Apple", "Banana", "Orange", "Mango"]
-#     return render_template('index.html', items=items)
-def index():
-    # username = 'John'
-    # date = '2022-01-01'
-    # items = ["Apple", "Banana", "Orange", "Mango"]
-    return render_template('index.html')
+@app.route('/data')
+def webapi():
+    return render_template('data.html')
+
+
+@app.route('/data/message', methods=['GET'])
+def getDataMessage():
+    if request.method == "GET":
+        with open('static/data/message.json', 'r') as f:
+            data = json.load(f)
+            print("text : ", data)
+        f.close
+        return jsonify(data)  # 直接回傳 data 也可以，都是 json 格式
+
+
+@app.route('/data/message', methods=['POST'])
+def setDataMessage():
+    if request.method == "POST":
+        data = {
+            'appInfo': {
+                'id': request.form['app_id'],
+                'name': request.form['app_name'],
+                'version': request.form['app_version'],
+                'author': request.form['app_author'],
+                'remark': request.form['app_remark']
+            }
+        }
+        print(type(data))
+        with open('static/data/message.json', 'w') as f:
+            json.dump(data, f)
+        f.close
+        return jsonify(result='OK')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
